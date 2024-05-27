e25d6c5e7de3242b5b5b80d307de72f4f2a10df0
Swpee <s.suwapee@gmail.com>
2024-05-27 12:18:17 +0700

    add: JavaScript Primitive Values Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productBahtPrice = 27000;
+let productBahtDiscount = productBahtPrice*0.25
+let productBahtNet = productBahtPrice-productBahtDiscount
